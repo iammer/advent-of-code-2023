7f6f63fe17227f6c52c73853a54d810643b2a566
Michael Ratliff <email@michaelratliff.com>
2023-12-07 01:21:12 +0000

    day6

---
+++ day06/day6b.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+EPSILON = 0.0000001
+
+times = [ARGF.readline.split[1..].join.to_f]
+distances = [ARGF.readline.split[1..].join.to_f]
+
+puts(times.zip(distances).map do |time, distance|
+  q = Math.sqrt(time * time + -4 * distance)
+
+  ((time + q) / 2 - EPSILON).floor - ((time - q) / 2 + EPSILON).ceil + 1
+end.inject(:*))
